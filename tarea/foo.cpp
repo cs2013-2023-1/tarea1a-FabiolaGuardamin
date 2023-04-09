a85d341c45e53a8e815c5252a7bd616c6f87b21e
FabiolaGuardamin <85091456+FabiolaGuardamin@users.noreply.github.com>
2023-04-09 11:40:55 -0500

    Add files via upload

--- tarea/foo.cpp
+++ tarea/foo.cpp
@@ -1,69 +1,78 @@
-#include "foo.h"
+//
+// Created by Fabiola Guardamino Morales
+//
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+class Matriz2D {
+private:
+    float filas;
+    float columnas;
+
+public:
+    Matriz2D();
+    Matriz2D(int n);
+    Matriz2D(int n, int m);
+    Matriz2D(const Matriz2D& m);
+    Matriz2D(Matriz2D&& m);
+
+    float getFilas();
+    float getColumnas();
+};
 
 Matriz2D::Matriz2D(){
-    // Constructor por defecto
+    srand(time(nullptr));
+    filas = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    columnas = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 }
 
 Matriz2D::Matriz2D(int n){
-    // Constructor con un parametro
+    srand(time(nullptr));
+    filas = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    columnas = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 }
 
 Matriz2D::Matriz2D(int n, int m){
-    // Constructor con dos parametros
+    srand(time(nullptr));
+    filas = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    columnas = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 }
 
 Matriz2D::Matriz2D(const Matriz2D& m){
-    // Constructor de copia
+    filas = m.filas;
+    columnas = m.columnas;
 }
 
 Matriz2D::Matriz2D(Matriz2D&& m){
-    // Constructor de movimiento
+    filas = m.filas;
+    columnas = m.columnas;
+    m.filas = 0;
+    m.columnas = 0;
 }
 
-Matriz2D t(Matriz2D& m){
-    // Transpuesta de una matriz
-}
-
-std::ostream& operator<<(std::ostream& os, const Matriz2D& m){
-    // Sobrecarga del operador <<
-}
-
-Matriz2D operator+(const Matriz2D& m1, const Matriz2D& m2){
-    // Sobrecarga del operador +
-}
-
-Matriz2D operator-(const Matriz2D& m1, const Matriz2D& m2){
-    // Sobrecarga del operador -
-}
-
-Matriz2D operator*(const Matriz2D& m1, const Matriz2D& m2){
-    // Sobrecarga del operador *
-}
-
-Matriz2D operator+(const Matriz2D& m, float n){
-    // Sobrecarga del operador +
-}
-
-Matriz2D operator-(const Matriz2D& m, float n){
-    // Sobrecarga del operador -
-}
-
-Matriz2D operator*(const Matriz2D& m, float n){
-    // Sobrecarga del operador *
-}
-
-Matriz2D operator/(const Matriz2D& m, float n){
-    // Sobrecarga del operador /
-}
-
-float Matriz2D::get(int i, int j){
-    return ptr[i][j];
-}
-
-int Matriz2D::getFilas(){
+float Matriz2D::getFilas(){
     return filas;
 }
 
-int Matriz2D::getColumnas(){
+float Matriz2D::getColumnas(){
     return columnas;
 }
+
+int main(){
+    Matriz2D matriz1;
+    Matriz2D matriz2(3);
+    Matriz2D matriz3(4, 5);
+    Matriz2D matriz4(matriz3);
+    Matriz2D matriz5(std::move(matriz4));
+
+    std::cout << "Matriz 1: " << matriz1.getFilas() << ", " << matriz1.getColumnas() << std::endl;
+    std::cout << "Matriz 2: " << matriz2.getFilas() << ", " << matriz2.getColumnas() << std::endl;
+    std::cout << "Matriz 3: " << matriz3.getFilas() << ", " << matriz3.getColumnas() << std::endl;
+    std::cout << "Matriz 4: " << matriz4.getFilas() << ", " << matriz4.getColumnas() << std::endl;
+    std::cout << "Matriz 5: " << matriz5.getFilas() << ", " << matriz5.getColumnas() << std::endl;
+
+    return 0;
+}
+
